369b1b2e4918aa3053d533477266b872522b4769
jeetiss <jeetiss@yandex.ru>
2020-03-10 10:49:13 +0300

    chore: update rollup plugins, remove unused

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 
 export default [
